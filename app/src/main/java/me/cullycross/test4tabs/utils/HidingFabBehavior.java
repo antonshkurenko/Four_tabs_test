f9c0861a8a353b0e8a5568042c41bb0556b826ac
CullyCross <elaugfein@gmail.com>
2015-11-05 14:03:01 +0200

    Fab hide behavior

---
+++ app/src/main/java/me/cullycross/test4tabs/utils/HidingFabBehavior.java
@@ -0,0 +1,45 @@
+package me.cullycross.test4tabs.utils;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by: Anton Shkurenko (cullycross)
+ * Project: Test4tabs
+ * Date: 11/5/15
+ * Code style: SquareAndroid (https://github.com/square/java-code-styles)
+ * Follow me: @tonyshkurenko
+ *
+ * See
+ * <a href="https://guides.codepath.com/android/Floating-Action-Buttons#using-coordinatorlayout">
+ *   this article</>
+ */
+public class HidingFabBehavior extends FloatingActionButton.Behavior {
+
+  public HidingFabBehavior(Context context, AttributeSet attrs) {
+    super();
+  }
+
+  @Override public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
+      FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+    return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(
+        coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+  }
+
+  @Override
+  public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+      View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+    super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
+        dyUnconsumed);
+
+    if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+      child.hide();
+    } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+      child.show();
+    }
+  }
+}
