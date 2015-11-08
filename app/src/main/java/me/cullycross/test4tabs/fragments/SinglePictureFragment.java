e751ce708ad4f9c336dfe8d15d5df3b3d479bbc2
CullyCross <elaugfein@gmail.com>
2015-11-08 19:26:47 +0200

    Final

--- app/src/main/java/me/cullycross/test4tabs/fragments/SinglePictureFragment.java
+++ app/src/main/java/me/cullycross/test4tabs/fragments/SinglePictureFragment.java
@@ -1,13 +1,22 @@
 package me.cullycross.test4tabs.fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import me.cullycross.test4tabs.R;
 
 public class SinglePictureFragment extends Fragment {
+
+  @Bind(R.id.image) ImageView mImage;
 
   public static SinglePictureFragment newInstance() {
     final SinglePictureFragment fragment = new SinglePictureFragment();
@@ -26,6 +35,45 @@ public class SinglePictureFragment extends Fragment {
   @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_single_picture, container, false);
+    View view = inflater.inflate(R.layout.fragment_single_picture, container, false);
+    ButterKnife.bind(this, view);
+    return view;
+  }
+
+  @Override public void onDestroyView() {
+    super.onDestroyView();
+    ButterKnife.unbind(this);
+  }
+
+  @OnClick(R.id.rotate) void rotate() {
+    final Matrix m = new Matrix();
+    final Bitmap bitmap = ((BitmapDrawable) mImage.getDrawable()).getBitmap();
+
+    m.postRotate(90);
+
+    final Bitmap rotatedBitmap =
+        Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), m, true);
+
+    mImage.setImageBitmap(rotatedBitmap);
+  }
+
+  @OnClick(R.id.flip_vertical) void flipVertical() {
+    final Matrix m = new Matrix();
+    final Bitmap bitmap = ((BitmapDrawable) mImage.getDrawable()).getBitmap();
+
+    m.preScale(1, -1);
+    final Bitmap flippedBitmap =
+        Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), m, false);
+    mImage.setImageBitmap(flippedBitmap);
+  }
+
+  @OnClick(R.id.flip_horizontal) void flipHorizontal() {
+    final Matrix m = new Matrix();
+    final Bitmap bitmap = ((BitmapDrawable) mImage.getDrawable()).getBitmap();
+
+    m.preScale(-1, 1);
+    final Bitmap flippedBitmap =
+        Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), m, false);
+    mImage.setImageBitmap(flippedBitmap);
   }
 }
